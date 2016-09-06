0365d4a54b34719af86c603aa79819bb537e62e3
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-06 12:16:24 +0100

    Add walking spider

---
+++ Assets/WalkingSpider.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkingSpider : MonoBehaviour {
+
+	public float speed = 1f;
+
+	private Rigidbody2D rb;
+	[SerializeField]
+	private Transform startPos, endPos;
+	private bool collision;
+
+	private void Awake () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	private void FixedUpdate () {
+		Move ();
+		ChangeDirection ();
+	}
+
+	private void OnCollisionEnter2D (Collision col) {
+		if (col.gameObject.tag == "Player") { 
+			PlayerController.instance.Die ();
+		}
+	}
+
+	private void Move () {
+		rb.velocity = new Vector2 (transform.localScale.x, 0) * speed;
+	}
+
+	private void ChangeDirection () {
+		collision = Physics2D.Linecast (startPos.position, endPos.position, 1 << LayerMask.NameToLayer ("ground"));
+		if (!collision) {
+			Vector3 temp = transform.localScale;
+			temp.x *= -1;
+			transform.localScale = temp;
+		}
+	}
+}
