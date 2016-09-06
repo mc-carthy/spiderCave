71b54c1fd118f3f0e788d73be9ed47a37843f21c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-06 13:30:45 +0100

    Menu creation

---
+++ Assets/LevelController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelController : MonoBehaviour {
+
+	public void LoadGame () {
+		SceneManager.LoadScene ("main");
+	}
+
+	public void BackToMenu () {
+		SceneManager.LoadScene ("menu");
+	}
+}
