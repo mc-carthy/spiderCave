50ecafc2118f22bfe1f190dcbfb87f94101848df
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-06 14:11:29 +0100

    Create camera follow script

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public float minX = -35f, maxX = 100f;
+
+	private Transform player;
+	private float offsetY;
+
+	private void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		offsetY = transform.position.y - player.position.y;
+	}
+
+	private void Update () {
+		if (player != null) {
+			Vector3 temp = transform.position;
+			temp.x = Mathf.Clamp (player.position.x, minX, maxX);
+			temp.y = player.position.y + offsetY;
+			transform.position = temp;
+		}
+	}
+}
